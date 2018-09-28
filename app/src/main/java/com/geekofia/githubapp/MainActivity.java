b2153387abe4f404f788a8f642673c38fd34b54c
geekofia <chandu.chandaniit@gmail.com>
2018-09-28 11:32:32 +0530

    Javascript enabled | issue #1 fixed

--- app/src/main/java/com/geekofia/githubapp/MainActivity.java
+++ app/src/main/java/com/geekofia/githubapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.geekofia.githubapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -14,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
 
         WebView mywebview = (WebView)findViewById(R.id.web1);
         mywebview.setWebViewClient(new WebViewClient());
-        mywebview.loadUrl("https://github.com");
+        WebSettings webSettings = mywebview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mywebview.loadUrl("https://github.com/");
     }
 }
